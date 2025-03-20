610691bec26f00557a61c7d1444c074dad478f6a
scmunoz000001 <scmunoz000001@gmail.com>
2025-03-19 23:58:31 -0500

    actualizando

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "./", // Asegura rutas relativas
   plugins: [react()],
   define: {
-    'process.env': process.env
+    'process.env': {} // Evita errores con process.env en Vite
   }
 })
+
